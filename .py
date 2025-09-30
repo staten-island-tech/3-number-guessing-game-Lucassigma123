cc72bfc6a90e00f5fb390832695c2eb75c7f7832
Lucassigma123 <lucass126@nycstudents.net>
2025-09-30 10:13:10 -0400

    Update .py

--- .py
+++ .py
@@ -2,14 +2,23 @@
 # randomint=random.randint(1,10)
 # print(randomint)
 
-import random
-integer= random.randint(1,10)
+# import random
+# integer= random.randint(1,25) #random randint is random integer
+# while True:
+#     guess= int(input("guess the number 1-25")) 
+#     if guess== integer:
+#         print("correct")
+#         print(integer)
+#         break
+#     else:print("incorrect")
+
+x=0
 while True:
-    guess= int(input("guess the number 1-10"))
-    if guess== integer:
-        print("correct")
-        print(integer)
-        break
-    else:print("incorrect")
-
-
+    y= int(input("guess number"))
+    if y ==x:
+         print ("you guess correctly")
+         break
+    if x>y:
+         print ("less than correct guess")
+         if y>x:
+              print ("greater than correct guess")
